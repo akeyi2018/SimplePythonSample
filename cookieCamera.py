e942526e34077f70664d3664616e9794f6a561cf
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-03-03 13:18:38 +0900

    PiCameraを使った写真撮影サンプル

--- cookieCamera.py
+++ cookieCamera.py
@@ -1,9 +1,11 @@
 import picamera
 
 # Setup the camera such that it closes
-print("About to take a picture")
+print("Start take a picture")
+
 with picamera.PiCamera() as camera:
     camera.resolution = (640,480)
     camera.capture("/home/pi/Desktop/newimage.jpg")
+    
 print("Picture taken")
 
