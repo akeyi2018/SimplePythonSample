f3a9cdbd3c6add797496803f0d219513d9c94663
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-03-04 18:23:17 +0900

    CPUコア数、CPU使用率、メモリ使用率、Macアドレスを出力するサンプル

--- GetCPUInfo.py
+++ GetCPUInfo.py
@@ -5,3 +5,9 @@ print("CPU Core:" + str(psutil.cpu_count()))
 print("CPU Percent: " + str(psutil.cpu_percent(interval=1)))
 
 print("Memory Used: " + str(psutil.virtual_memory().percent))
+
+#mac = psutil.net_if_addrs()["eth0"][-1][1]
+
+mac = psutil.net_if_addrs()["lo"][-1][1]
+
+print("Mac Address: " + str(mac))
