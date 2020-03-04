8a9ccc7b9cb72318d2532609bb465cd37c0b5a6e
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-03-04 17:49:49 +0900

    CPUコア数、CPU使用率、メモリ使用率を出力するサンプル

--- GetCPUInfo.py
+++ GetCPUInfo.py
@@ -2,6 +2,6 @@ import psutil
 
 print("CPU Core:" + str(psutil.cpu_count()))
 
-print("CPU Percent: " + str(psutil.cpu_percent()))
+print("CPU Percent: " + str(psutil.cpu_percent(interval=1)))
 
 print("Memory Used: " + str(psutil.virtual_memory().percent))
