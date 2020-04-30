9ca8e2ea29fb3e9670adb69553e50c13918983ce
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-04-30 15:45:44 +0900

    Update and rename servoBlasterTest.py to Servo_gpiozero.py

---
+++ Servo_gpiozero.py
@@ -0,0 +1,21 @@
+from gpiozero import Servo, Button
+from time import sleep
+
+flg = 0
+button = Button(20, hold_time = 0.1)
+
+def setServoState(flg):
+    servo = Servo(21)
+    if flg == 0:
+        servo.min()
+    else:
+        servo.max()
+    sleep(0.2)
+
+while True:
+    button.wait_for_press()
+    if flg == 0:
+        flg = 1
+    else:
+        flg = 0
+    setServoState(flg)
